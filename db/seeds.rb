a9609b995739bd5d1c71eeeb81464a900eb0a908
Marcin Szot <marcin@Marcins-MacBook-Pro-2.local>
2017-08-04 00:24:25 +0200

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(title: "Topic #{topic+1}")
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
       title: "My blog post #{blog}",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      topic_id: Topic.last.id
   )
 end
 
@@ -29,11 +36,22 @@ puts "5 skills created"
 9.times do |portfolio_item|
   Portfolio.create!(
       title: "Portfolio title #{portfolio_item}",
-      subtitle: "My great service",
+      subtitle: "Ruby on Rails",
       body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio",
       main_image: "http://via.placeholder.com/600x400",
       thumb_image: "http://via.placeholder.com/300x200"
     )
 end 
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+      title: "Portfolio title #{portfolio_item}",
+      subtitle: "Angular",
+      body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio",
+      main_image: "http://via.placeholder.com/600x400",
+      thumb_image: "http://via.placeholder.com/300x200"
+    )
+end 
+
 
 puts "9 portfolio items created"
