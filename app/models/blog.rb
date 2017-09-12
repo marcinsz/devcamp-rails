2d8360ab2cd932b4801e7e4548dd69677e272c33
Marcin Szot <marcin@Marcins-MacBook-Pro-2.local>
2017-09-13 00:00:24 +0200

    Added validations for topic in blogs, added tipic select in blog form

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,8 +4,9 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
 
   has_many :comments, dependent: :destroy 
+  belongs_to :topic
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
 
   # scope :published, ->{where(status: 'published').order('created_at DESC')}
   scope :recent , -> {order('status DESC, created_at DESC')}
